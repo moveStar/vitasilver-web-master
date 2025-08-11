c522ee4ea76fca5f5ef4734ee12a2e7790497c57
张维娟 <15002518569@163.com>
2025-08-11 16:21:43 +0800

    first commit

---
+++ src/config/style.ts
@@ -0,0 +1,22 @@
+/*
+ * @Description:
+ * @Author: zhangweijuan
+ * @Date: 2025-06-16 23:26:06
+ * @LastEditTime: 2025-08-11 14:28:02
+ */
+export default {
+  showFooter: true,
+  isSidebarCompact: false,
+  showBreadcrumb: false,
+  menuAutoCollapsed: true,
+  mode: 'auto',
+  layout: top,
+  splitMenu: false,
+  sideMode: 'light',
+  isFooterAside: false,
+  isSidebarFixed: true,
+  isHeaderFixed: true,
+  isUseTabsRouter: false,
+  showHeader: true,
+  brandTheme: '#00A870',
+};
